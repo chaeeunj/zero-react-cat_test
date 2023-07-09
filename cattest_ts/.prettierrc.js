4171bf2ccb79c3dd01c9bd96404d6df4d8d4543f
chaeeunj <official2930@naver.com>
2023-07-09 11:28:46 +0900

    개발환경 설정

---
+++ cattest_ts/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "singleQuote": true,
+  "semi": true,
+  "trailingComma": "all",
+  "arrowParens": "always",
+  "orderedImports": true,
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "arrowParens": "avoid",
+};
